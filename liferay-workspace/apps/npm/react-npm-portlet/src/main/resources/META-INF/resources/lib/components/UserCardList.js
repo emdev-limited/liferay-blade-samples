42384e5269c130fbd5d42ec24d22c8d907b8acda
tigonin <tigonin@emdev.ru>
2020-12-29 18:19:23 +0700

    INC-1113: pagination

--- liferay-workspace/apps/npm/react-npm-portlet/src/main/resources/META-INF/resources/lib/components/UserCardList.js
+++ liferay-workspace/apps/npm/react-npm-portlet/src/main/resources/META-INF/resources/lib/components/UserCardList.js
@@ -7,11 +7,12 @@ import ClayAlert from '@clayui/alert';
 
 export default function UserCardList() {
 
-let [page, setPage] = React.useState(1);
+  let [currentPage, setCurrentPage] = React.useState(1);
+  let [portionNumber, setPortionNumber] = React.useState(1);
 
   const ALL_USERS = gql`
   query{
-    userAccounts(page: ${page}) {
+    userAccounts(page: ${currentPage}, pageSize: ${5}) {
       items {
         name,
         alternateName,
@@ -24,7 +25,7 @@ let [page, setPage] = React.useState(1);
   }
   `;
 
-  const { loading, error, data } = useQuery(ALL_USERS);
+  const { loading, error, data} = useQuery(ALL_USERS);
 
   if (loading) return <ClayLoadingIndicator />;
   if (error) {
@@ -38,7 +39,12 @@ let [page, setPage] = React.useState(1);
     );
   }
 
-  if (data.userAccounts.items.length === 0) {
+  let {items} = data.userAccounts;
+  let {totalCount} = data.userAccounts;
+  let {pageSize} = data.userAccounts;
+  let {page} = data.userAccounts;
+
+  if (items.length === 0) {
     return (
       <ClayAlert displayType="info" className="text-center">
         No users.
@@ -46,15 +52,54 @@ let [page, setPage] = React.useState(1);
     );
   }
 
-  const cards = data.userAccounts.items.map(
-    ({name, id, alternateName}) => (
-      <UserCard
-        key={id}
-        name={name}
-        alternateName={alternateName}
-      />
-    )
+  const cards = items.map(
+      ({name, id, alternateName}) => (
+          <UserCard
+            key={id}
+            name={name}
+            alternateName={alternateName}
+          />
+      )
   );
 
-  return <div className="row">{cards}</div>;
+    let pagesCount = Math.ceil(totalCount / pageSize);
+    let pages = [];
+
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
+    }
+
+    let portionSize = 3;
+    let portionCount = Math.ceil(pagesCount / portionSize);
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+    let rightPortionPageNumber  = portionNumber * portionSize;
+
+  return <React.Fragment> <div className="row">{cards}</div>
+
+    <hr/>
+    <div>
+        { portionNumber > 1 && <span className={'icon-angle'} onClick={() => {
+            setPortionNumber(portionNumber - 1);
+            setCurrentPage(leftPortionPageNumber - portionSize);
+        }}>{<svg className="lexicon-icon lexicon-icon-angle-left" focusable="false" role="presentation" viewBox="0 0 512 512">
+            <path className="lexicon-icon-outline" d="M114.106 254.607c0.22 6.936 2.972 13.811 8.272 19.11l227.222 227.221c11.026 11.058 28.94 11.058 39.999 0 11.058-11.026 11.058-28.94 0-39.999l-206.333-206.333c0 0 206.333-206.333 206.333-206.333 11.058-11.059 11.058-28.973 0-39.999-11.058-11.059-28.973-11.059-39.999 0l-227.221 227.221c-5.3 5.3-8.052 12.174-8.273 19.111z"></path>
+        </svg>}</span>}
+
+        <b>{pages.filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+            .map((p, idx) => {
+                return <span className={p === page
+                    ? 'usersPaginationBtns currenPage'
+                    :'usersPaginationBtns'}
+                             key={idx}
+                             onClick={() => setCurrentPage(p)}>{p}</span>
+            })}</b>
+
+        {portionCount > portionNumber && <span className={'icon-angle'} onClick={() => {
+            setPortionNumber(portionNumber + 1);
+            setCurrentPage(rightPortionPageNumber + 1);
+        }}>{<svg className="lexicon-icon lexicon-icon-angle-right" focusable="false" role="presentation" viewBox="0 0 512 512">
+            <path className="lexicon-icon-outline" d="M396.394 255.607c-0.22-6.936-2.973-13.81-8.272-19.111l-227.221-227.221c-11.026-11.059-28.94-11.059-39.999 0-11.058 11.026-11.058 28.941 0 39.999l206.333 206.333c0 0-206.333 206.333-206.333 206.333-11.058 11.058-11.058 28.973 0 39.999 11.059 11.059 28.972 11.059 39.999 0l227.221-227.221c5.3-5.3 8.053-12.175 8.272-19.111z"></path>
+        </svg>}</span> }
+    </div>
+  </React.Fragment>
 }
